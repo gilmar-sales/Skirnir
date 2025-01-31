ec3c4aed4359ea79c6c73038f48914972d86f075
gilmar-sales <gilmar.custodio@outlook.com.br>
2025-01-30 22:24:32 -0300

    Add support to add singleton with service factory

--- include/ServiceCollection.hpp
+++ include/ServiceCollection.hpp
@@ -1,209 +1,240 @@
 #pragma once
 
 #include <any>
+#include <cassert>
 #include <functional>
 #include <memory>
-#include <cassert>
 
+#include "Common.hpp"
 #include "Reflection.hpp"
 #include "ServiceId.hpp"
 #include "ServiceProvider.hpp"
-#include "Common.hpp"
 
-class ServiceCollection {
-public:
-    ServiceCollection()
-        : mServiceDefinitionMap(std::make_shared<ServiceDefinitionMap>()) {
-    };
+class ServiceCollection
+{
+  public:
+    ServiceCollection() :
+        mServiceDefinitionMap(std::make_shared<ServiceDefinitionMap>()) {};
 
     ~ServiceCollection() = default;
 
-    template<typename TService>
-        requires(std::tuple_size_v<refl::as_tuple<TService> > > 0)
-    ServiceCollection &AddSingleton() {
+    template <typename TService>
+    ServiceCollection& AddSingleton(const ServiceFactory& factory)
+    {
+        AddServiceWithFactory<TService, TService>(LifeTime::Singleton, factory);
+
+        return *this;
+    }
+
+    template <typename TContract, typename TService>
+        requires(std::is_base_of_v<TContract, TService>)
+    ServiceCollection& AddSingleton(const ServiceFactory& factory)
+    {
+        AddServiceWithFactory<TContract, TService>(LifeTime::Singleton,
+                                                   factory);
+
+        return *this;
+    }
+
+    template <typename TService>
+        requires(std::tuple_size_v<refl::as_tuple<TService>> > 0)
+    ServiceCollection& AddSingleton()
+    {
         AddServiceWithConstructorArgs<TService, TService>(LifeTime::Singleton);
 
-
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddSingleton() {
+    template <typename TService>
+    ServiceCollection& AddSingleton()
+    {
         AddService<TService, TService>(LifeTime::Singleton);
 
+        return *this;
+    }
+
+    template <typename TService>
+    ServiceCollection& AddSingleton(std::shared_ptr<TService> element)
+    {
+        AddServiceWithInstance<TService, TService>(element,
+                                                   LifeTime::Singleton);
 
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddSingleton(std::shared_ptr<TService> element) {
-        AddServiceWithInstance<TService, TService>(element, LifeTime::Singleton);
-
-
-        return *this;
-    }
-
-    template<typename TContract, typename TService>
-        requires(not std::is_same_v<TContract, TService> and std::is_base_of_v<TContract, TService>)
-    ServiceCollection &AddSingleton(std::shared_ptr<TService> element) {
-        AddServiceWithInstance<TContract, TService>(element, LifeTime::Singleton);
-
+    template <typename TContract, typename TService>
+        requires(not std::is_same_v<TContract, TService> and
+                 std::is_base_of_v<TContract, TService>)
+    ServiceCollection& AddSingleton(std::shared_ptr<TService> element)
+    {
+        AddServiceWithInstance<TContract, TService>(element,
+                                                    LifeTime::Singleton);
 
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddTransient(const ServiceFactory &factory) {
+    template <typename TService>
+    ServiceCollection& AddTransient(const ServiceFactory& factory)
+    {
         AddServiceWithFactory<TService, TService>(LifeTime::Transient, factory);
 
+        return *this;
+    }
+
+    template <typename TContract, typename TService>
+        requires(std::is_base_of_v<TContract, TService>)
+    ServiceCollection& AddTransient(const ServiceFactory& factory)
+    {
+        AddServiceWithFactory<TContract, TService>(LifeTime::Transient,
+                                                   factory);
 
         return *this;
     }
 
-    template<typename TContract, typename TService>
+    template <typename TContract, typename TService>
         requires(std::is_base_of_v<TContract, TService>)
-    ServiceCollection &AddTransient(const ServiceFactory &factory) {
-        AddServiceWithFactory<TContract, TService>(LifeTime::Transient, factory);
-
-
-        return *this;
-    }
-
-    template<typename TContract, typename TService>
-        requires(std::is_base_of_v<TContract, TService>)
-    ServiceCollection &AddTransient() {
+    ServiceCollection& AddTransient()
+    {
         AddService<TContract, TService>(LifeTime::Transient);
 
-
         return *this;
     }
 
-    template<typename TService>
-        requires(std::tuple_size_v<refl::as_tuple<TService> > > 0)
-    ServiceCollection &AddTransient() {
+    template <typename TService>
+        requires(std::tuple_size_v<refl::as_tuple<TService>> > 0)
+    ServiceCollection& AddTransient()
+    {
         AddServiceWithConstructorArgs<TService, TService>(LifeTime::Transient);
 
-
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddTransient() {
+    template <typename TService>
+    ServiceCollection& AddTransient()
+    {
         AddService<TService, TService>(LifeTime::Transient);
 
-
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddScoped(const ServiceFactory &factory) {
+    template <typename TService>
+    ServiceCollection& AddScoped(const ServiceFactory& factory)
+    {
         AddServiceWithFactory<TService, TService>(LifeTime::Scoped, factory);
 
-
         return *this;
     }
 
-    template<typename TContract, typename TService>
+    template <typename TContract, typename TService>
         requires(std::is_base_of_v<TContract, TService>)
-    ServiceCollection &AddScoped(const ServiceFactory &factory) {
+    ServiceCollection& AddScoped(const ServiceFactory& factory)
+    {
         AddServiceWithFactory<TContract, TService>(LifeTime::Scoped, factory);
 
-
         return *this;
     }
 
-    template<typename TContract, typename TService>
+    template <typename TContract, typename TService>
         requires(std::is_base_of_v<TContract, TService>)
-    ServiceCollection& AddScoped() {
+    ServiceCollection& AddScoped()
+    {
         AddService<TContract, TService>(LifeTime::Scoped);
 
-
         return *this;
     }
 
-    template<typename TService>
-        requires(std::tuple_size_v<refl::as_tuple<TService> > > 0)
-    ServiceCollection &AddScoped() {
+    template <typename TService>
+        requires(std::tuple_size_v<refl::as_tuple<TService>> > 0)
+    ServiceCollection& AddScoped()
+    {
         AddServiceWithConstructorArgs<TService, TService>(LifeTime::Scoped);
 
         return *this;
     }
 
-    template<typename TService>
-    ServiceCollection &AddScoped() {
+    template <typename TService>
+    ServiceCollection& AddScoped()
+    {
         AddService<TService, TService>(LifeTime::Scoped);
 
         return *this;
     }
 
-    template<typename TService>
-    [[nodiscard]] bool Contains() const {
-        return mServiceDefinitionMap->contains(
-            GetServiceId<TService>());
+    template <typename TService>
+    [[nodiscard]] bool Contains() const
+    {
+        return mServiceDefinitionMap->contains(GetServiceId<TService>());
     }
 
-    [[nodiscard]] std::shared_ptr<ServiceProvider> CreateServiceProvider() const {
+    [[nodiscard]] std::shared_ptr<ServiceProvider> CreateServiceProvider() const
+    {
         return std::make_shared<ServiceProvider>(mServiceDefinitionMap);
     }
 
-protected:
-    template<typename TContract, typename TService>
-    void AddService(const LifeTime lifeTime) {
+  protected:
+    template <typename TContract, typename TService>
+    void AddService(const LifeTime lifeTime)
+    {
         assert(!Contains<TContract>() && "Can't register twice");
 
-        mServiceDefinitionMap->insert({
-            GetServiceId<TContract>(), {
-                .factory = [](ServiceProvider &) { return std::make_shared<TService>(); },
-                .lifetime = lifeTime
-            }
-        });
+        mServiceDefinitionMap->insert(
+            { GetServiceId<TContract>(),
+              { .factory =
+                    [](ServiceProvider&) {
+                        return std::make_shared<TService>();
+                    },
+                .lifetime = lifeTime } });
     }
 
-    template<typename TContract, typename TService>
-        requires(std::tuple_size_v<refl::as_tuple<TService> > > 0)
-    void AddServiceWithConstructorArgs(const LifeTime lifeTime) {
+    template <typename TContract, typename TService>
+        requires(std::tuple_size_v<refl::as_tuple<TService>> > 0)
+    void AddServiceWithConstructorArgs(const LifeTime lifeTime)
+    {
         assert(!Contains<TContract>() && "Can't register twice");
 
         using ConstructorArgs = refl::as_tuple<TService>;
 
-        mServiceDefinitionMap->insert({
-            GetServiceId<TContract>(), {
-                .factory = CreateServiceFactory<TService>(ConstructorArgs{}),
-                .lifetime = lifeTime
-            }
-        });
+        mServiceDefinitionMap->insert(
+            { GetServiceId<TContract>(),
+              { .factory  = CreateServiceFactory<TService>(ConstructorArgs {}),
+                .lifetime = lifeTime } });
     }
 
-    template<typename TContract, typename TService>
-    void AddServiceWithFactory(const LifeTime lifeTime, const ServiceFactory &factory) {
+    template <typename TContract, typename TService>
+    void AddServiceWithFactory(const LifeTime        lifeTime,
+                               const ServiceFactory& factory)
+    {
         assert(!Contains<TContract>() && "Can't register twice");
 
         mServiceDefinitionMap->insert(
-            {GetServiceId<TContract>(), {.factory = factory, .lifetime = lifeTime}});
+            { GetServiceId<TContract>(),
+              { .factory = factory, .lifetime = lifeTime } });
     }
 
-    template<typename TContract, typename TService>
-    void AddServiceWithInstance(std::shared_ptr<TService> instance, const LifeTime lifeTime) {
+    template <typename TContract, typename TService>
+    void AddServiceWithInstance(std::shared_ptr<TService> instance,
+                                const LifeTime            lifeTime)
+    {
         assert(!Contains<TContract>() && "Can't register twice");
 
-        mServiceDefinitionMap->insert({
-            GetServiceId<TContract>(),
-            {
-                .factory = [instance = instance](ServiceProvider &) { return instance; },
-                .lifetime = lifeTime
-            }
-        });
+        mServiceDefinitionMap->insert(
+            { GetServiceId<TContract>(),
+              { .factory = [instance = instance](
+                               ServiceProvider&) { return instance; },
+                .lifetime = lifeTime } });
     }
 
-    template<typename TService, typename... Args>
+    template <typename TService, typename... Args>
         requires(std::is_constructible_v<TService, Args...>)
-    ServiceFactory CreateServiceFactory(std::tuple<Args...> tuple) {
-        return ServiceFactory([](ServiceProvider &serviceProvider) {
+    ServiceFactory CreateServiceFactory(std::tuple<Args...> tuple)
+    {
+        return ServiceFactory([](ServiceProvider& serviceProvider) {
             return std::make_shared<TService>(
-                serviceProvider.GetService<std::remove_pointer_t<decltype(Args(nullptr).get())> >()...);
+                serviceProvider.GetService<
+                    std::remove_pointer_t<decltype(Args(nullptr).get())>>()...);
         });
     }
 
-private:
+  private:
     std::shared_ptr<ServiceDefinitionMap> mServiceDefinitionMap;
 };
